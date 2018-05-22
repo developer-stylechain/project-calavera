b234966fe18f8f5b3f63f7027dbabf2a5059ed13
Schalk Neethling <sneethling@mozilla.com>
2018-05-22 12:45:43 +0200

    feat((skeletons)): Add support for common bundle
    Add support for common bundle. Add two new functions to add an Array of dependecies and write
    multiple files

--- index.js
+++ index.js
@@ -1,66 +1,77 @@
 'use strict';
 
-const { addDependency, getNPMInstallCmd } = require('./lib/utils');
+const {
+    addDependency,
+    addDependencies,
+    getNPMInstallCmd
+} = require('./lib/utils');
 const { getConfig } = require('./lib/config');
 const { writeFile, writeFiles } = require('./lib/fileUtils');
 
 const COMMON = [
-  'CODE-OF-CONDUCT.md',
-  '.eslintrc.js',
-  '.prettierrc',
-  '.sasslintrc',
-  '.stylelintrc',
-  '.stylelintignore'
+    'CODE-OF-CONDUCT.md',
+    '.eslintrc.js',
+    '.prettierrc',
+    '.sasslintrc',
+    '.stylelintrc',
+    '.stylelintignore'
 ];
+const COMMON_DEPENDECIES = ['eslint', 'prettier', 'sass-lint', 'stylelint'];
 
 async function fillCatacomb() {
-  const calaveraConfig = await getConfig();
-  let dependecies = [];
+    const calaveraConfig = await getConfig();
+    let dependecies = [];
 
-  for (let config in calaveraConfig) {
-    switch (config) {
-      case 'all-contributors':
-        writeFile('.all-contributorsrc');
-        dependecies = addDependency('all-contributors-cli', dependecies);
-        break;
-      case 'code-of-conduct':
-        writeFile('CODE-OF-CONDUCT.md');
-        break;
-      case 'contribute-json':
-        writeFile('contribute.json');
-        break;
-      case 'common':
-        writeFiles(COMMON);
-        break;
-      case 'prettier':
-        writeFile('.prettierrc');
-        dependecies = addDependency('prettier', dependecies);
-        break;
-      case 'eslint':
-        writeFile('.eslintrc.js');
-        dependecies = addDependency('eslint', dependecies);
-        break;
-      case 'readme':
-        writeFile('README.md');
-        break;
-      case 'sass-lint':
-        writeFile('.sasslintrc');
-        dependecies = addDependency('sass-lint', dependecies);
-        break;
-      case 'stylelint':
-        writeFiles(['.stylelintrc', '.stylelintignore']);
-        dependecies = addDependency('stylelint', dependecies);
-        break;
-      default:
-        console.error(`No configuration or skeleton match found for ${config}`);
-        break;
+    for (let config in calaveraConfig) {
+        switch (config) {
+            case 'all-contributors':
+                writeFile('.all-contributorsrc');
+                dependecies = addDependency(
+                    'all-contributors-cli',
+                    dependecies
+                );
+                break;
+            case 'code-of-conduct':
+                writeFile('CODE-OF-CONDUCT.md');
+                break;
+            case 'contribute-json':
+                writeFile('contribute.json');
+                break;
+            case 'common':
+                writeFiles(COMMON);
+                dependecies = addDependencies(COMMON_DEPENDECIES, dependecies);
+                break;
+            case 'prettier':
+                writeFile('.prettierrc');
+                dependecies = addDependency('prettier', dependecies);
+                break;
+            case 'eslint':
+                writeFile('.eslintrc.js');
+                dependecies = addDependency('eslint', dependecies);
+                break;
+            case 'readme':
+                writeFile('README.md');
+                break;
+            case 'sass-lint':
+                writeFile('.sasslintrc');
+                dependecies = addDependency('sass-lint', dependecies);
+                break;
+            case 'stylelint':
+                writeFiles(['.stylelintrc', '.stylelintignore']);
+                dependecies = addDependency('stylelint', dependecies);
+                break;
+            default:
+                console.error(
+                    `No configuration or skeleton match found for ${config}`
+                );
+                break;
+        }
     }
-  }
 
-  console.info(
-    'Run the following command to install dependencies: ',
-    getNPMInstallCmd(dependecies)
-  );
+    console.info(
+        'Run the following command to install dependencies: ',
+        getNPMInstallCmd(dependecies)
+    );
 }
 
 fillCatacomb();
