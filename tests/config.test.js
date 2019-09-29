784182e4fa1eb767a13e49cadf7ffe0e13a22262
Schalk Neethling <sneethling@mozilla.com>
2019-09-29 19:48:44 +0200

    feat(project): add support for sass linting config
    Add a sasslint configuration for stylelint

---
+++ tests/config.test.js
@@ -0,0 +1,36 @@
+const mock = require("mock-fs");
+const { getConfig } = require("../lib/config");
+
+describe("config", () => {
+  afterEach(() => {
+    mock.restore();
+  });
+
+  test("config loads package.json and returns calavera object", async () => {
+    mock({
+      "package.json": JSON.stringify({
+        name: "project-calavera",
+        version: "0.0.0-semantically-released",
+        calavera: {
+          commonjs: true
+        }
+      })
+    });
+    const calaveraConfig = await getConfig();
+
+    expect(calaveraConfig).toBeDefined();
+    expect(calaveraConfig).toMatchObject({ commonjs: true });
+  });
+
+  test("config loads package.json and returns undefined when there is no calavera config", async () => {
+    mock({
+      "package.json": JSON.stringify({
+        name: "project-calavera",
+        version: "0.0.0-semantically-released"
+      })
+    });
+    const calaveraConfig = await getConfig();
+
+    expect(calaveraConfig).toBeUndefined();
+  });
+});
